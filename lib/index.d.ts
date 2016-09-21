0c7218006d6e33d739c6c946fdd3820805f2d2eb
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 11:44:05 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import GaeaModel from './gaea-model/gaea-model.component';
+import { PropsDefine as GaeaModelPropsDefine } from './gaea-model/gaea-model.type';
+export { GaeaModel, GaeaModelPropsDefine };
+export default GaeaModel;
