6d754776beca4b88a11ad7934e199c0bb854a5e5
huangziyi01 <huangziyi01@baidu.com>
2016-09-22 11:37:08 +0800

    发布组件

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../../../components/editor/gaea-model/gaea-model/application.d.ts" />
+/// <reference path="../../../components/editor/gaea-model/gaea-model/viewport.d.ts" />
+/// <reference path="../../../components/editor/gaea-model/gaea-model/event.d.ts" />
+/// <reference path="../../../components/editor/gaea-model/gaea-model/diff.d.ts" />
